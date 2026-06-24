e53eb46e676af1c5bb3b3839fc58f5dbeafd3e82
Nishtha Sethi <nishthasethi004@gmail.com>
2026-06-24 10:47:26 +0530

    Eating Queries

---
+++ DSA/Array/CF1676E.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m;
+    cin>>m;
+    while(m--){
+        int n, q;
+        cin>>n>>q;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        sort(arr, arr+n, greater<int>());
+        for(int i = 1; i < n; i++){
+            arr[i]+=arr[i-1];
+        }
+        while(q--){
+            int x;
+            cin >> x;
+            int ans = lower_bound(arr, arr+n, x) - arr;
+            if(ans == n){
+                cout << -1 << endl;
+            }
+            else{
+                cout << ans + 1 << endl;
+            }
+        }
+    }
+
+}
