4d5cffa4107d3a7074776e63b1c17c54ea4252ac
Nishtha Sethi <nishthasethi004@gmail.com>
2026-07-01 10:16:03 +0530

    Bit Manipulation

--- DSA/Array/Ques.cpp
+++ DSA/Array/Ques.cpp
@@ -1,3 +1,31 @@
 // Binary Search on ans
 
 // https://www.spoj.com/problems/EKO/
+
+//Bit Manipulation
+// Swapping numbers without using third variable
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    a = a +b;
+    b = a - b;
+    a = a - b;
+    cout << a << " " << b << endl;
+}
+
+// Using XOR operator
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    a = a ^ b;
+    b = b ^ a;
+    a = a ^ b;
+    cout << a << " " << b << endl;
+}
+
+// Bit Masking
