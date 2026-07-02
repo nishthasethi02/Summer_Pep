0a7222f01dd663606de95ffd7541cf2004116ed9
Nishtha Sethi <nishthasethi004@gmail.com>
2026-07-02 11:15:06 +0530

    Linked List

--- DSA/Linked List/01.cpp
+++ DSA/Linked List/01.cpp
@@ -17,6 +17,93 @@ void print(Node* head){
         temp = temp->next;
 }
 */
+
+//Insertion at end
+void insertAtEnd(Node* &head, int data){
+    Node* newNode = new Node(data);
+    if(head == NULL){
+        head = newNode;
+        head->next = NULL;
+    }
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = NULL;
+}
+
+// Insertion at Position
+void insertAtPosition(Node* &head, int data, int position){
+    Node* newNode = new Node(data);
+    if(position == 1){
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    for(int i = 1; i < position - 1; i++){
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+// Insertion at Beginning
+void insertAtBeginning(Node* &head, int data){
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+//Deletion at end
+void deleteAtEnd(Node* &head){
+    if(head == NULL){
+        return;
+    }
+    if(head->next == NULL){
+        delete head;
+        head = NULL;
+        return;
+    }
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+}
+
+// Deletion at Position
+void deleteAtPosition(Node* &head, int position){
+    if(head == NULL){
+        return;
+    }
+    if(position == 1){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+    Node* temp = head;
+    for(int i = 1; i < position - 1; i++){
+        temp = temp->next;
+    }
+    Node* nodeToDelete = temp->next;
+    temp->next = nodeToDelete->next;
+    delete nodeToDelete;
+}
+
+// Deletion at Beginning
+void deleteAtBeginning(Node* &head){
+    if(head == NULL){
+        return;
+    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
 int main(){
     Node* head = new Node(10);
     Node* second = new Node(20);
@@ -24,6 +111,11 @@ int main(){
     head->next = second;
     second->next = third;
     // print(head);
+    insertAtEnd(head, 40);
+    insertAtPosition(head, 25, 3);
+    deleteAtEnd(head);
+    deleteAtBeginning(head);
+    deleteAtPosition(head, 2);
     Node* current = head;
     while(current != NULL){
         cout << current->data << " ";
--- DSA/Linked List/Ques.cpp
+++ DSA/Linked List/Ques.cpp
@@ -1,1 +1,49 @@
-// 
+// Reverse the linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = NULL;
+    Node* current = head;
+    Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    head->next = second;
+    second->next = third;
+
+    head = reverseList(head);
+    Node* current = head;
+    
+    cout << "Reversed List: ";
+    current = head;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+
+    return 0;
+}
