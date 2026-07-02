e2861d6769951fd92bec3bef430b2821ab6ec3b0
Nishtha Sethi <nishthasethi004@gmail.com>
2026-07-02 11:51:42 +0530

    Linked List

--- DSA/Linked List/Ques.cpp
+++ DSA/Linked List/Ques.cpp
@@ -46,3 +46,46 @@ int main(){
 
     return 0;
 }
+
+// Cycle detection in linked list
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void detectCycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            cout << "Cycle detected in the linked list." << endl;
+            return;
+        }
+    }
+
+    cout << "No cycle detected in the linked list." << endl;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    head->next = second;
+    second->next = third;
+    third->next = second; // Creating a cycle for testing
+
+    detectCycle(head);
+
+    return 0;
+}
